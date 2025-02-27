509f12086dacabae734de6295472f0f60bad9f7c
arc-research-analytics <123026629+arc-research-analytics@users.noreply.github.com>
2025-02-27 19:35:09 +0000

    Deploying to gh-pages from @ arc-research-analytics/gdot_pipeline@a7a3560bf34e4a78526b91588b9838568ebfd210 🚀

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // swap out with pk.eyJ1Ijoid3dyaWdodDIxIiwiYSI6ImNtN2MwdjdtYjBqeTUycnBwbHI1cWJrZmIifQ.BztD8jx6SLKxOtjK1ae4kg before deployment
-mapboxgl.accessToken =
-  "pk.eyJ1Ijoid3dyaWdodDIxIiwiYSI6ImNtN2MwdjdtYjBqeTUycnBwbHI1cWJrZmIifQ.BztD8jx6SLKxOtjK1ae4kg";
+mapboxgl.accessToken = "pk.eyJ1Ijoid3dyaWdodDIxIiwiYSI6ImNtN2MwdjdtYjBqeTUycnBwbHI1cWJrZmIifQ.BztD8jx6SLKxOtjK1ae4kg";
 
 // instantiate map
 const map = new mapboxgl.Map({
